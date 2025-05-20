d4dcf7b94f4a148e3a3de15a3b10e9c76d628789
GabrielDumith <gabriel.dumith321@gmail.com>
2025-05-20 12:44:57 -0300

    desafio

---
+++ main.py
@@ -0,0 +1,18 @@
+from biblioteca import *
+
+nadador = Nadador(70)
+nadador.Atletaaposentado()
+nadador.aquecerAtleta()
+nadador.nadar()
+
+ciclista = Ciclista(70)
+ciclista.Atletaaposentado()
+ciclista.aquecerAtleta()
+ciclista.pedalar()
+
+triatleta = Triatleta(70)
+triatleta.Atletaaposentado()
+triatleta.aquecerAtleta()
+triatleta.correr()
+triatleta.nadar()
+triatleta.pedalar()
